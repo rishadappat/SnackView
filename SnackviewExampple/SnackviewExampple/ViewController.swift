3c4809e6074ec464fe8087b31dc3f1f198ae876a
Rishad Appat <rishadappat@gmail.com>
2022-02-09 10:36:24 +0400

    master commit

--- SnackviewExampple/SnackviewExampple/ViewController.swift
+++ SnackviewExampple/SnackviewExampple/ViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import SnackView
 
 class ViewController: UIViewController {
 
--- Sources/SnackView/SnackViewBulder.swift
+++ Sources/SnackView/SnackViewBulder.swift
@@ -40,56 +40,58 @@ class SnackViewBulder: NSObject {
     
     private func showSnackView(withMessage message: String, bgColor color: UIColor, fontColor textColor: UIColor, iconName iconImg: String, autoHide: Bool)
     {
-        let view = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
-        self.timer?.invalidate()
-        var snackviewHeight: CGFloat = 50
-        self.snackView = UIView.init(frame: CGRect.init(x: 16, y: view!.frame.height + 8, width: view!.frame.size.width - 32, height: snackviewHeight))
-        
-        self.snackView.cornerRadius = self.cornerRadius
-        self.snackView.shadowRadius = 8
-        self.snackView.shadowOpacity = 0.8
-        self.snackView.borderWidth = 1
-        self.snackView.borderColor = color
-        self.snackView.shadowOffset = CGSize.init(width: 0, height: 0)
-        
-        let messageLabel = UILabel.init(frame: CGRect.init(x: 16, y: 8, width: self.snackView.frame.size.width - 52, height: snackviewHeight))
-        messageLabel.textAlignment = .natural
-        messageLabel.text = message
-        messageLabel.numberOfLines = 0
-        messageLabel.lineBreakMode = .byWordWrapping
-        messageLabel.textColor = textColor
-        messageLabel.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
-        messageLabel.sizeToFit()
-        snackviewHeight = messageLabel.frame.height + 16 >= 50 ? messageLabel.frame.height + 16 : 50
-        self.snackView.frame.size.height = snackviewHeight
-        
-        messageLabel.Y = (self.snackView.frame.height/2) - (messageLabel.frame.height/2)
-        
-        let messageImage = UIImageView.init(frame: CGRect.init(x: self.snackView.frame.width - 40, y: (self.snackView.frame.size.height/2) - 15, width: 30, height: 30))
-        messageImage.contentMode = .scaleAspectFit
-        messageImage.image = UIImage.init(named: iconImg)
-        messageImage.backgroundColor = .clear
-        
-        let blurEffectView: UIVisualEffectView = {
-            let blurEffect = UIBlurEffect(style: .dark)
-            let blurEffectView = UIVisualEffectView(effect: blurEffect)
-            let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
-            let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
-            blurEffectView.contentView.addSubview(vibrancyView)
-            blurEffectView.backgroundColor = color.withAlphaComponent(0.8)
-            return blurEffectView
-        }()
-        blurEffectView.frame = self.snackView.bounds
-        blurEffectView.cornerRadius = self.cornerRadius
-        self.snackView.addSubview(blurEffectView)
-        self.snackView.addSubview(messageLabel)
-        self.snackView.addSubview(messageImage)
+        if let view = UIApplication.shared.keyWindows
+        {
+            self.timer?.invalidate()
+            var snackviewHeight: CGFloat = 50
+            self.snackView = UIView.init(frame: CGRect.init(x: 16, y: view.frame.height + 8, width: view.frame.size.width - 32, height: snackviewHeight))
+            
+            self.snackView.cornerRadius = self.cornerRadius
+            self.snackView.shadowRadius = 8
+            self.snackView.shadowOpacity = 0.8
+            self.snackView.borderWidth = 1
+            self.snackView.borderColor = color
+            self.snackView.shadowOffset = CGSize.init(width: 0, height: 0)
+            
+            let messageLabel = UILabel.init(frame: CGRect.init(x: 16, y: 8, width: self.snackView.frame.size.width - 52, height: snackviewHeight))
+            messageLabel.textAlignment = .natural
+            messageLabel.text = message
+            messageLabel.numberOfLines = 0
+            messageLabel.lineBreakMode = .byWordWrapping
+            messageLabel.textColor = textColor
+            messageLabel.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+            messageLabel.sizeToFit()
+            snackviewHeight = messageLabel.frame.height + 16 >= 50 ? messageLabel.frame.height + 16 : 50
+            self.snackView.frame.size.height = snackviewHeight
+            
+            messageLabel.Y = (self.snackView.frame.height/2) - (messageLabel.frame.height/2)
+            
+            let messageImage = UIImageView.init(frame: CGRect.init(x: self.snackView.frame.width - 40, y: (self.snackView.frame.size.height/2) - 15, width: 30, height: 30))
+            messageImage.contentMode = .scaleAspectFit
+            messageImage.image = UIImage.init(named: iconImg)
+            messageImage.backgroundColor = .clear
+            
+            let blurEffectView: UIVisualEffectView = {
+                let blurEffect = UIBlurEffect(style: .dark)
+                let blurEffectView = UIVisualEffectView(effect: blurEffect)
+                let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
+                let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
+                blurEffectView.contentView.addSubview(vibrancyView)
+                blurEffectView.backgroundColor = color.withAlphaComponent(0.8)
+                return blurEffectView
+            }()
+            blurEffectView.frame = self.snackView.bounds
+            blurEffectView.cornerRadius = self.cornerRadius
+            self.snackView.addSubview(blurEffectView)
+            self.snackView.addSubview(messageLabel)
+            self.snackView.addSubview(messageImage)
+        }
     }
     
     func show()
     {
         self.hideSnackViewCallback { [weak self] in
-            if let view = UIApplication.shared.windows.filter({$0.isKeyWindow}).first
+            if let view = UIApplication.shared.keyWindows
             {
                 if let self = self
                 {
